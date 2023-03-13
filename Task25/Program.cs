5eb3980ec6e2582d0ba151d56779fa078996b19d
Your Name <7706530@gmail.com>
2023-03-13 19:56:46 +0300

    HomeWork_5

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,18 +1,17 @@
-﻿// // Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
-// и возводит число A в натуральную степень B.
-
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-Console.WriteLine("Введите число а: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int degreeNumber = DegreeNumber(number);
-Console.WriteLine($"Возведете а в натуральную степень {number} * {degreeNumber} ");
-int DegreeNumber(int a, int b) 
-    { 
-        for (int i = 0; i < b; i++) 
-        { 
-            a *= a; 
-        } 
-        return a; 
-    } 
+Console.Write("Введите число А:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberB < 0) Console.WriteLine("Число B не может быть отрицательным!");
+else Console.Write($"{numberA} в {numberB} степени = {Exponentiation(numberA, numberB)}.");
+
+int Exponentiation(int nmbrA_value, int nmbrB_value)
+{
+    int result = 1;
+    for (int i = 0; i < nmbrB_value; i++) result *= nmbrA_value;
+    return result;
+}
---
+++ Task27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{number} -> {SumDigits(number)}.");
+
+int SumDigits(int number_value)
+{
+    int buffer = number_value, sum = 0;
+    while (buffer > 0)
+    {
+        sum += buffer % 10; 
+        buffer /= 10;
+    }
+    return sum;
+}

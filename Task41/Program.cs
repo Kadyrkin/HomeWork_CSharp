7420b07f67310af0443fa72d996d25f2b8999a92
Your Name <7706530@gmail.com>
2023-03-19 22:27:41 +0300

    HomeWork7

---
+++ Task41/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа через запятую: ");
+
+string[] strNumbers = Console.ReadLine().Split(",");
+
+Console.WriteLine(Count(strNumbers));
+
+int Count(string[] strNumbers)
+{
+    int cnt = 0; for (int i = 0; i < strNumbers.Length; i++)
+    {
+        if (Convert.ToInt32(strNumbers[i].Trim()) > 0)
+        {
+            cnt++;
+        }
+    }
+    return cnt;
+}
---
+++ Task49/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+
+void PrintMatrix(int[,] matrix)
+
+void ReplaceNumberToSquare(int[,] matrix)
+{
+      for (int i = 0; i < matrix.GetLength(0); i++)
+      {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i < matrix.GetLength(0))
+            {
+                i = i%2;
+            }
+        }
+      }
+
+}
